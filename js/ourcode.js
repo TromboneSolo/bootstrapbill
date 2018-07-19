5930201478ccc7aecf074016af24a60acd4f0dc5
micah.sorkin <micahsorkin@protonmail.com>
2018-07-18 18:57:37 -0700

    fixed some stuff and added ourcode

--- js/ourcode.js
+++ js/ourcode.js
@@ -1,11 +1,11 @@
 function showtext(){
     var mytext= $("#searchbox").val();
     $.getJSON('https://owapi.net/api/v3/u/' + mytext + '/blob', function(data) {
-        //$("img").each(function(){
-            //var src=$(this).attr("src", data.us.stats.quickplay.overall_stats.avatar);
-        //});
-        //$("#avatar").src = data.us.stats.quickplay.overall_stats.avatar;
-       //alert(data.us.stats.quickplay.overall_stats.avatar);
+        $("img").each(function(){
+            var src=$(this).attr("src", data.us.stats.quickplay.overall_stats.avatar);
+        });
+        $("#avatar").src = data.us.stats.quickplay.overall_stats.avatar;
+       alert("this user's SR is " + data.us.stats.quickplay.overall_stats.comprank + "!");
           console.log(data);
     });
 }
