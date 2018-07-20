ff40e3ea8d8a50aa7c5119b91b19af9c67279f07
micah.sorkin <micahsorkin@protonmail.com>
2018-07-19 23:23:15 -0700

    made first li change text to "all damage done" property from API

--- js/ourcode.js
+++ js/ourcode.js
@@ -1,10 +1,15 @@
 function showtext(){
     var mytext= $("#searchbox").val();
     $.getJSON('https://owapi.net/api/v3/u/' + mytext + '/blob', function(data) {
-        $("img").each(function(){
+        
+        $("#transformicon").each(function(){
             var src=$(this).attr("src", data.us.stats.quickplay.overall_stats.avatar);
         });
-        $("#avatar").src = data.us.stats.quickplay.overall_stats.avatar;
+
+        $("#alldamagedone").text("All Damage Done: " + data.us.stats.quickplay.game_stats.all_damage_done);
+        
+        //$("#avatar").src = data.us.stats.quickplay.overall_stats.avatar;
+
        alert("this user's SR is " + data.us.stats.quickplay.overall_stats.comprank + "!");
           console.log(data);
     });
